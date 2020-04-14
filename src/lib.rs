adcfd2cea04ca93759dcf9899cd7b429c138ba4b
Tobin Yehle <tobinyehle@gmail.com>
2020-04-14 12:00:39 -0700

    finish example peripheral calls

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 #![no_std]
 
-use embedded_hal;
+use embedded_hal as hal;
 use nb::block;
+use void;
 
 #[derive(Debug)]
 pub enum TError<S, P> {
@@ -16,12 +17,11 @@ const SPI_BYTES: usize = NUM_COLS / 8;
 // type SPI = dyn embedded_hal::spi::FullDuplex<u8, Error=()>;
 
 pub struct LEDArray {
-    array: [[u8; NUM_COLS]; NUM_ROWS],
+    pub array: [[u8; NUM_COLS]; NUM_ROWS],
     // spi: SPI,
 }
 
 impl LEDArray {
-
     pub fn prepare_row(&self, r: usize, buf: &mut [[u8; SPI_BYTES]; 4]) {
         assert!(r < NUM_ROWS);
         let row = self.array[r];
@@ -43,59 +43,58 @@ impl LEDArray {
                 output = output >> 8;
             }
         }
-
     }
 }
 
-// pub fn timing<T>(timer: T)
-// where T: embedded_hal::timer::CountDown {
-//     timer.start(1);
-//     // do some other stuff
-//     (0..10_000).sum::<i32>();
-//     // wait for the timer to finish
-//     block!(timer.wait());
-// }
-
-pub fn testing<P, S>(pin: &mut P, spi: &mut S) -> Result<(), TError<S::Error, P::Error>>
-where P: embedded_hal::digital::v2::OutputPin,
-      S: embedded_hal::spi::FullDuplex<u8>
+pub fn timing<T, D>(timer: &mut T, base_delay: D) -> Result<(), void::Void>
+where
+    T: hal::timer::CountDown,
+    T::Time: core::convert::From<D>,
+    D: core::ops::Mul<Output = D>,
+    i32: core::convert::Into<D>,
 {
-    pin.set_low().map_err(TError::Pin)?;
-    block!(spi.send(0u8)).map_err(TError::Serial)?;
+    timer.start(base_delay * (1 << 3).into());
+    // do some other stuff
+    (0..10_000).sum::<i32>();
+    // wait for the timer to finish
+    block!(timer.wait())
+}
+
+pub fn spi<S>(bus: &mut S) -> Result<(), S::Error>
+where
+    S: hal::spi::FullDuplex<u8>,
+{
+    for &data in &[7, 5, 1] {
+        block!(bus.send(data))?;
+    }
     Ok(())
 }
 
-
-pub fn go() -> i32 {
-    42
+pub fn pins<P>(a: &mut P, b: &mut P) -> Result<(), P::Error>
+where
+    P: hal::digital::v2::OutputPin,
+{
+    a.set_high()?;
+    b.set_low()?;
+    Ok(())
 }
-
 
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
-    fn test_go() {
-        assert_eq!(go(), 42);
-    }
-
-    #[test]
     fn test_prepare_row() {
         let row = [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
 
-        let array = LEDArray {
-            array: [row; 8],
-        };
+        let array = LEDArray { array: [row; 8] };
 
         let mut buf = [[0u8; 2]; 4];
         array.prepare_row(0, &mut buf);
 
-        // println!("{:08b}, {:08b}", buf[0][0], buf[0][1]);
-
-        assert_eq!(buf[0], [0b10101010, 0b10101010]);  // for 1s
-        assert_eq!(buf[1], [0b11001100, 0b11001100]);  // for 2s
-        assert_eq!(buf[2], [0b11110000, 0b11110000]);  // for 4s
-        assert_eq!(buf[3], [0b11111111, 0b00000000]);  // for 8s
+        assert_eq!(buf[0], [0b10101010, 0b10101010]); // for 1s
+        assert_eq!(buf[1], [0b11001100, 0b11001100]); // for 2s
+        assert_eq!(buf[2], [0b11110000, 0b11110000]); // for 4s
+        assert_eq!(buf[3], [0b11111111, 0b00000000]); // for 8s
     }
 }
